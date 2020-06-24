cb32a675add2a78121ed32ef881ec32f5d877f5a
Martin <m.beleckii@alef.im>
2020-06-24 13:01:36 +0300

    Initialize vue project and connect it to express

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const admin = require('firebase-admin');
+const express = require('express');
 const serviceAccount = require('./keys/stolen-bikes-127db-firebase-adminsdk-nxcbl-84cce71d82.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/vue/dist/index.html`);
+});
+
+app.use('/', express.static(`${__dirname}/vue/dist`));
+
+app.listen(3000);
 
 // [DB] Bikes -> id, number, status
 // [DB] Officers -> id, bike_id, status
