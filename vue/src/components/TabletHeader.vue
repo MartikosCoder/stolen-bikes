82ea6cf2a23a8f824f113b32c60d4829050d0484
Martin <m.beleckii@alef.im>
2020-06-24 21:30:44 +0300

    Styles for header

--- vue/src/components/TabletHeader.vue
+++ vue/src/components/TabletHeader.vue
@@ -11,7 +11,7 @@
           tabindex="0"
           v-model="mode_selected"
         />
-        <label class="switcher-label" for="switcher"></label>
+        <span class="slider round"></span>
       </div>
     </div>
   </header>
@@ -34,4 +34,66 @@ export default {
 </script>
 
 <style>
+header, .mode {
+  display: flex;
+}
+
+header {
+  padding: 10px;
+  border-bottom: 2px solid #2196f3;
+}
+
+.mode {
+  justify-content: space-between;
+  align-items: center;
+  width: 100px;
+}
+.switcher {
+  position: relative;
+  display: inline-block;
+  width: 44px;
+  height: 25px;
+}
+
+.switcher-checkbox {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #2196F3;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 21px;
+  width: 21px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.switcher-checkbox:checked + .slider:before {
+  transform: translateX(19px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
 </style>
